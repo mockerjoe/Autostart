cea930c9c1423acf6838ab4c97a0c3e9d45dedd9
mockerjoe <mocker.joe@web.de>
2022-05-09 18:03:28 +0200

    Add files via upload

--- start_box.py
+++ start_box.py
@@ -11,15 +11,18 @@ def main():
         work()
 
 def game():
-    subprocess.Popen('D:\\Emlulator\\DS4windows\\DS4Windows.exe') #DS4 controller support
-    subprocess.Popen('D:\\Emlulator\\publish\\Ryujinx.exe') #Switch emulator
+    subprocess.Popen('C:\\Program Files\\Sublime Text\\sublime_text.exe')
+    #subprocess.Popen('D:\\Emlulator\\DS4windows\\DS4Windows.exe') #DS4 controller support
+    #subprocess.Popen('D:\\Emlulator\\publish\\Ryujinx.exe') #Switch emulator
+    root.destroy()
 
 def work():
-    subprocess.Popen('D:\\JetBrains\\IntelliJ IDEA 2021.2.3\\bin\\idea64.exe') #IntelliJ IDEA
+    #subprocess.Popen('D:\\JetBrains\\IntelliJ IDEA 2021.2.3\\bin\\idea64.exe') #IntelliJ IDEA
     #subprocess.Popen('D:\\JetBrains\\IntelliJ IDEA 2021.2.3\\bin\\idea64.exe') #PyCharm
-    subprocess.Popen('D:\\Sublime Text\\sublime_text.exe') #Sublime text
-    #subprocess.Popen('C:\\Program Files\\Mozilla Firefox\\Firefox.exe') #Firefox
+    #subprocess.Popen('D:\\Sublime Text\\sublime_text.exe') #Sublime text
+    subprocess.Popen('C:\\Program Files\\Mozilla Firefox\\Firefox.exe') #Firefox
     #webbrowser.open_new_tab('https://studip.hochschule-trier.de', new=2, autoraise=True)
+    root.destroy()
 
 #Create Window
 root = Tk()
@@ -27,10 +30,10 @@ root.title("SwaGGerAuto$tart")
 root.geometry("375x175")
 
 #Button for gaming
-gameButton = ttk.Button(root, text="Gaming", command=game()).place(x=50, y=125)
+gameButton = ttk.Button(root, text="Gaming", command=game).place(x=50, y=125)
 
 #Button for working
-workButton = ttk.Button(root, text="Working", command=work()).place(x=150, y=125)
+workButton = ttk.Button(root, text="Working", command=work).place(x=150, y=125)
 
 #Button for default start
 defButton = ttk.Button(root, text="Default", command=root.destroy).place(x=250, y=125)
