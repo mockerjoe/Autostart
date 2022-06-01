b43678bf5a423af1337dc6f2dc64f3d0cbbae85e
mockerjoe <mocker.joe@web.de>
2022-06-01 04:56:28 +0200

    Add files via upload

---
+++ Pc-Start/og files/browser.py
@@ -0,0 +1,83 @@
+import tkinter as tk
+from tkinter import *
+from tkinter import filedialog
+import os
+
+checkgame = os.path.exists("game.txt")
+
+if checkgame == False:
+	my_file = open("game.txt", "w+")
+	print("Datei game.txt wurde erzeugt.")
+else:
+	print("Datei bereits vorhanden.")
+
+def browseFilesGame():
+	filename = filedialog.askopenfilename(initialdir = "/",
+										title = "Select a File",
+										filetypes = (("Text files", "*.txt*"), ("all files", "*.*")))
+	
+	# Change label contents
+	#label_file_explorer.configure(text="File Opened: " + filename)
+	
+	myText = open(r'game.txt','a')
+	myString = filename
+	myText.write("subprocess.Popen('" + myString + "')" + "\r\n")
+	myText.close()
+	
+def browseFilesWork():
+	filename = filedialog.askopenfilename(initialdir = "/",
+										title = "Select a File",
+										filetypes = (("Text files", "*.txt*"), ("all files", "*.*")))
+	
+	# Change label contents
+	#label_file_explorer.configure(text="File Opened: " + filename)
+	
+	myText = open(r'work.txt','a')
+	myString = filename
+	myText.write("subprocess.Popen('" + myString + "')" + "\r\n")
+	myText.close()
+	
+# Create the window
+browser = Tk()
+
+# Set window title
+browser.title('skrr skrr engine v0.2')
+
+# Set browser icon
+browser.tk.call("wm", "iconphoto", browser._w, tk.PhotoImage(file="icons/find.png"))
+
+# Set window size
+browser.geometry("530x200")
+browser.resizable(False, False)
+
+#Set window background color
+browser.config(background = "white")
+
+# Create a File Explorer label
+label_file_explorer = Label(browser, text = "File Explorer auf Tkiner Basis",
+								width = 75, height = 4, fg = "blue")
+
+	
+button_explore_game = Button(browser, text = "Browse Files for game",
+							command = browseFilesGame)
+
+button_explore_work = Button(browser, text = "Browse Files for work",
+							command = browseFilesWork)
+
+button_exit = Button(browser, text = "Exit",
+						command = exit)
+
+# Grid method is chosen for placing
+# the widgets at respective positions
+# in a table like structure by
+# specifying rows and columns
+label_file_explorer.grid(column = 1, row = 1)
+
+button_explore_game.grid(column = 1, row = 2)
+
+button_explore_work.grid(column = 1, row = 3)
+
+button_exit.grid(column = 1,row = 4)
+
+# Let the window wait for any events
+browser.mainloop()
---
+++ Pc-Start/og files/start_box.py
@@ -0,0 +1,111 @@
+import subprocess, webbrowser, os
+import tkinter as tk
+from tkinter import *
+from tkinter import ttk
+from tkinter import filedialog
+
+checkgame = os.path.exists("game.txt")
+checkwork = os.path.exists("work.txt")
+
+# Function for Button
+def game():
+
+    # Remove empty lines from text file
+    with open("game.txt") as reader, open("game.txt", "r+") as writer:
+        for line in reader:
+            if line.strip():
+                writer.write(line)
+            writer.truncate()
+
+    # Remove blank file paths
+    try:
+        with open("game.txt", "r") as fr:
+            lines = fr.readlines()
+
+            with open("game.txt", "w") as fw:
+                for line in lines:
+
+                    if line.strip("\n") != "subprocess.Popen('')":
+                        fw.write(line)
+        print("gelöscht!")
+    except:
+        print("Es wurde nichts entfernt.")
+
+    if checkgame == False:                  #Check if file or path exists
+        my_file = open("game.txt","w+")     #Create file if it doest exist  
+    else:    
+        with open("game.txt", "r") as game:
+            for line in game:
+                exec(line)                  #Open file if it already exists (only works with .exe currently)
+    
+    root.destroy()
+
+def work():
+
+    if checkwork == False:                  #Check if file or path exists
+        my_file = open("work.txt","w+")     #Create file if it doest exist
+        print("Datei work.txt wurde erzeugt.")
+    else:
+        # Remove empty lines from text file
+        with open("work.txt") as reader, open("work.txt", "r+") as writer:
+            for line in reader:
+                if line.strip():
+                    writer.write(line)
+                writer.truncate()
+
+        # Remove blank file paths
+        try:
+            with open("work.txt", "r") as fr:
+                lines = fr.readlines()
+
+                with open("work.txt", "w") as fw:
+                    for line in lines:
+
+                        if line.strip("\n") != "subprocess.Popen('')":
+                            fw.write(line)
+            print("gelöscht!")
+        except:
+            print("Es wurde nichts entfernt.")
+
+        with open("work.txt", "r") as work:
+            for line in work:
+                exec(line)      #Open file if it already exists
+
+    root.destroy()
+
+def browseFiles():
+    exec(open("C:/Users/ziggu/Desktop/Python stuff/File reader/browser.py").read())
+
+# Create Window
+root = tk.Tk()
+root.title("GinGaXTREME_Setup  v0.3") # GinGaXTREME_Setup, SwaGGerAuto$tart, $ugerbahbii, PuchachiMunanyoo 
+root.geometry("475x175")
+root.resizable(False, False)
+
+
+# Set Window background color
+#root.config(background = "blue")
+
+# Button for gaming
+gameButton = ttk.Button(root, text="Gaming", command=game).place(x=50, y=125)
+
+# Button for working
+workButton = ttk.Button(root, text="Working", command=work).place(x=150, y=125)
+
+# Button for default start
+defButton = ttk.Button(root, text="Default", command=root.destroy).place(x=350, y=125)
+
+# Button for 
+editButton = ttk.Button(root, text="Edit", command=browseFiles).place(x=250, y=125)
+
+# Label for Window
+Label(root, text="Wähle dein Startprofil: \n1. Gaming \n2. Work \n3. Default", font=('Consolas 15')).pack()
+
+# Timer to kill Programm after set amount of ms
+root.after(20000, lambda: root.destroy())
+
+# Set icon for window
+root.tk.call("wm", "iconphoto", root._w, tk.PhotoImage(file="icons/startup.png"))
+
+# Run window
+root.mainloop()
